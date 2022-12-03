ab7b54e71e9424e74a9ec01f9a449917031823f3
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-12-03 17:02:06 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -65,7 +65,7 @@ class TitleState extends MusicBeatState
 	{
 		
 		
-		#if windows && html)
+		#if (windows && html)
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
 		#end
